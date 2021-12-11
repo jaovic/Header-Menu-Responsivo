ed89ad4a76e297bb91957899f76b6a1143ba02a9
João Victor Martins <48113822+jaovic@users.noreply.github.com>
2021-12-10 23:04:32 -0300

    Update script.js

--- script.js
+++ script.js
@@ -6,6 +6,12 @@ function toggleMenu(event) {
   nav.classList.toggle('active');
  const active = nav.classList.contains('active');
   event.currentTarget.setAttribute('aria-expanded', active);
+  if (active) {
+    event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+  } else {
+    event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+  }
+}
  
 
 btnMobile.addEventListener('click', toggleMenu);
